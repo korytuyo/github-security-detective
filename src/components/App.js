0ec34b6c323459637eb189aa29b12baf453403df
Kory Tuyo <korytuyo@live.com>
2025-07-09 18:19:17 -0500

    Init add

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to GitHub Advanced Security Game</h1>
+        <p>Get ready to learn and have fun!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
